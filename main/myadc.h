befcfb6eef6e0f8b1b85ab3a09ca0196ee70ab9d
root <root@redmibook>
2025-01-13 22:06:12 +0800

    adc分支初始化

---
+++ main/myadc.h
@@ -0,0 +1,7 @@
+#ifndef __MYADC_H__
+#define __MYADC_H__
+
+bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
+void example_adc_calibration_deinit(adc_cali_handle_t handle);
+
+#endif
